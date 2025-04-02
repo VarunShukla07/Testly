98c55456e1bd829095ccc0eaa00e21244ee87144
VarunShukla07 <varun.shuklaoffical@gmail.com>
2025-04-02 23:07:51 +0530

    Setup

---
+++ testly/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod,
+        nisl eget ultricies ultricies, nunc nisl ultricies nunc, nec ultricies
+        nisl nisl nec nisl.
+      </p>
+    </div>
+  )
+}
+
+export default page
